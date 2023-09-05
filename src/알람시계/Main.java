0eaa143b65e0621824ac64a94ec996e3d6997df6
jojo6807 <jyj051811@gmail.com>
2023-09-05 21:08:59 +0900

    오븐 알람

--- src/알람시계/Main.java
+++ src/알람시계/Main.java
@@ -1,5 +1,5 @@
 package 알람시계;
-// 전날 다음날 생각할 필요는 없는데 한번 해본거, 다시 해보자 좀 지저분해서
+// 다음날은 % 사용하면 다음날까지는 되지만, 다다음날은 안됨
 import java.util.Scanner;
 
 // 알람 시계 45분 문제
@@ -10,21 +10,20 @@ public class Main {
         int H = sc.nextInt();
         System.out.println("분 입력 :");
         int M = sc.nextInt();
+        
         int total = H * 60 + M;
-        int h = (total - 45) / 60;
+        int h = (total - 45)/ 60;
         int m = (total - 45) % 60;
-        System.out.println("알람이 울립니다.");
-        if (h <= 0) {
+
+        if (h == 0) {
             h = 23;
-            m = M + 15;
-            System.out.println("전날" + h + " : " + m);
-        } else if (h > 24) {
-            h = (total / 60) / 24;
-            System.out.println("다음날" + h + " : " + m);
-        } else if (m - 45 < 0) {
-            h -= 1;
-            m += 15;
-            System.out.println(h + "  : " + m);
         }
+        if (m < 45) {
+            m += 60;
+        }
+        if (h > 24) {
+            h = ((total - 45)/ 60) % 24;
+        }
+        System.out.println(h + " : " + m);
     }
 }
--- src/오븐시계/Main.java
+++ src/오븐시계/Main.java
@@ -1,7 +1,25 @@
 package 오븐시계;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
+// 현재 시간 출력
+// 걸린 시간 더하기
+// 시간은 분단위로 주어짐
+// 24시가 되면 00시로 초기화
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("현재시간 입력");
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+        System.out.println(H + " " + M );
 
+        System.out.println("조리시간(분) :");
+        int time = sc.nextInt();
+        int h = time / 60;
+        int m = time % 60;
+
+        System.out.println((H + h) % 24 + " " + (M + m));   // 24시가 되면 0시로 초기화
     }
 }
