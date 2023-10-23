0edb6aa26578e0ea137f9f6c439e9549cb5ca99c
jojo6807 <jyj051811@gmail.com>
2023-10-23 22:55:56 +0900

    꼬마정민

---
+++ src/꼬마정민/Main.java
@@ -0,0 +1,13 @@
+package 꼬마정민;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(a+b+c);
+    }
+}
