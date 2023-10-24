532fdede372d3c7c542a9d90fa7be7bff473a999
jojo6807 <jyj051811@gmail.com>
2023-10-24 23:19:35 +0900

    성적 계산기

--- src/시험성적/Main.java
+++ src/시험성적/Main.java
@@ -6,6 +6,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int score = sc.nextInt();
-        (score >= 90) ? System.out.println("A") : (score >= 80) ? System.out.println("B") : (score >= 70) ? System.out.println("C") : (score >= 60) ? System.out.println("D") : (score >= 50) ? System.out.println("F") : System.out.println("다시 입력하시오");
+        if ((score >= 90)) {
+            System.out.println("A");
+        } else {
+            if ((score >= 80)) {
+                System.out.println("B");
+            } else {
+                if ((score >= 70)) {
+                    System.out.println("C");
+                } else {
+                    if ((score >= 60)) {
+                        System.out.println("D");
+                    } else {
+                        if ((score >= 50)) {
+                            System.out.println("F");
+                        } else {
+                            System.out.println("다시 입력하시오");
+                        }
+                    }
+                }
+            }
+        }
     }
 }
