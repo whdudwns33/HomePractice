90328a089299a5dfc2766d808a4c3a0422a55a08
jojo6807 <jyj051811@gmail.com>
2023-10-26 22:52:14 +0900

    오븐 시계

--- src/오븐시계/Main.java
+++ src/오븐시계/Main.java
@@ -1,25 +1,18 @@
 package 오븐시계;
 
-import java.time.LocalDate;
 import java.util.Scanner;
 
-// 현재 시간 출력
-// 걸린 시간 더하기
-// 시간은 분단위로 주어짐
-// 24시가 되면 00시로 초기화
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("현재시간 입력");
-        int H = sc.nextInt();
-        int M = sc.nextInt();
-        System.out.println(H + " " + M );
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        int t = sc.nextInt();
+        int total = h*60 + m + t;
 
-        System.out.println("조리시간(분) :");
-        int time = sc.nextInt();
-        int h = (time / 60);
-        int m = time % 60;
-
-        System.out.println((H + h) % 24 + " " + (M + m));   // 24시가 되면 0시로 초기화
+        int H = (total / 60) % 24;
+        int M = total % 60;
+        System.out.println(H + " " + M);
     }
 }
