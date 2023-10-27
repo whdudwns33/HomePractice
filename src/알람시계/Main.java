c4f9627ada751450d7a9cd7324d7009cadec1ee4
jojo6807 <jyj05181@gmail.com>
2023-10-27 19:38:10 +0900

    주사위 예제. 중요한게 리스트 만드는 방법

--- src/알람시계/Main.java
+++ src/알람시계/Main.java
@@ -1,29 +1,22 @@
 package 알람시계;
-// 다음날은 % 사용하면 다음날까지는 되지만, 다다음날은 안됨
+
 import java.util.Scanner;
 
-// 알람 시계 45분 문제
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("시간 입력 :");
-        int H = sc.nextInt();
-        System.out.println("분 입력 :");
-        int M = sc.nextInt();
-        
-        int total = H * 60 + M;
-        int h = (total - 45)/ 60;
-        int m = (total - 45) % 60;
-
-        if (h == 0) {
-            h = 23;
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        if(m < 45) {
+            h--;		// 시(hour) 1 감소
+            m= 60 - (45 - m); 	// 분(min) 감소
+            if(h < 0) {
+                h = 23;
+            }
+            System.out.println(h + " " + h);
         }
-        if (m < 45) {
-            m += 60;
+        else {
+            System.out.println(h + " " + (h - 45));
         }
-        if (h > 24) {
-            h = ((total - 45)/ 60) % 24;
-        }
-        System.out.println(h + " : " + m);
     }
 }
--- src/주사위세개/Main.java
+++ src/주사위세개/Main.java
@@ -1,51 +1,30 @@
 package 주사위세개;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        int random1 = (int) (Math.random() * 6 + 1);
-        int random2 = (int) (Math.random() * 6 + 1);
-        int random3 = (int) (Math.random() * 6 + 1);
-        int total;
-        System.out.println(random1 + " " + random2 + " " + random3);
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(a);
+        arr.add(b);
+        arr.add(c);
 
-        if (random1 == random2 && random2 == random3) { // 다 같은 경우
-            total = 10000 + random3 * 1000;
-            System.out.println(total);
-
-        } else if (random1 == random2) {    // 1과 2가 같고 1과 3이 다름
-            if (random1 != random3) {
-                total = 1000 + random1 * 1000;
-                System.out.println(total);
-            }
-        }else if (random1 == random3) {
-            if (random1 != random2) {
-                total = 1000 + random1 * 1000;
-                System.out.println(total);
-            }
-        } else if (random2 == random3) {
-            if (random1 != random2) {
-                total = 1000 + random2 * 1000;
-                System.out.println(total);
+        for (int i = 0; i < 3; i++) {
+            if (arr.get(i) > )
         }
 
-    } else if (random1 != random2 && random2 != random3 && random1 != random3) {    // 다 다른 경우
-            if (random1 > random2) {
-                if (random1 > random3) {
-                    total = random1 * 100;
-                    System.out.println(total);
-                } else if (random1 < random3) {
-                    total = random3 * 100;
-                    System.out.println(total);
-                } else;
-            } else if (random1 < random2) {
-                if (random2 > random3) {
-                    total = random2 * 100;
-                    System.out.println(total);
-                } else if (random2 < random3) {
-                    total = random3 * 100;
-                    System.out.println(total);
-                } else;
-            }
+
+
+        int total = 0;
+        if (a == b && a == c) {
+            total = 10000 + 1000*a;
+        } else if (a != b && a!= c && b != c) {
+            total =
         }
+
     }
 }
